b9912f1a083d19a847e1cb55012eb9fc1378b7e9
MARCELA <yulybeltran082@gmail.com>
2021-07-02 17:47:35 -0500

    Modifique archivos

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import Perros from './Perros';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header/>
         <Nav/>
        
